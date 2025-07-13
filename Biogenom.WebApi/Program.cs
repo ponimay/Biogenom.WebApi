2bf044e3541f95626844ff3539c97d4163ce7681
ponimay <kirpichnikov.nik2019@yandex.ru>
2025-07-14 01:56:05 +0300

    fix bugs

--- Biogenom.WebApi/Program.cs
+++ Biogenom.WebApi/Program.cs
@@ -5,12 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://+:80");
 builder.Services.AddDbContext<Biogenom.WebApi.Models.BiogenomDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<Biogenom.WebApi.Models.BiogenomDbContext>();
+    db.Database.EnsureCreated();
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
